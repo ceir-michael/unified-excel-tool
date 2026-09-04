434cc806750c3d4551daf4d88b445f97723f5a0e
Michael A. <m.ainabe89@gmail.com>
2026-09-04 18:45:57 -0500

    version update

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 APP_NAME = "Unified Excel Tools"
-APP_VERSION = "0.2.0-beta"
+APP_VERSION = "0.3.0-beta"
 APP_ORGANIZATION = "Cizik School of Nursing - CEIR"
 APP_REPOSITORY = "ceir-michael/unified-excel-tool"
 
